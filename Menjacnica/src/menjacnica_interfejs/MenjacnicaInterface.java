2a549f2e77c736281fe91d888964d08c9b4cda58
Sofija Dimitrijevic <dimitrijevic.sofijaa@gmail.com>
2018-03-20 15:52:50 +0100

    Napravljena grana specifikacijainterfejsa. Napravljen interface MenjacnicaInterface.

---
+++ Menjacnica/src/menjacnica_interfejs/MenjacnicaInterface.java
@@ -0,0 +1,12 @@
+package menjacnica_interfejs;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.Kurs;
+import menjacnica.Valuta;
+
+public interface MenjacnicaInterface {
+      public void dodavanjeKursa(Valuta valuta, Kurs kurs);
+      public void brisanjeKursa(Valuta valuta, GregorianCalendar datum);
+      public Kurs pronalazenjeKursaValute(String naziv, GregorianCalendar datum);
+}
