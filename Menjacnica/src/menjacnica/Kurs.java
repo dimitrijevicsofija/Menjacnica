11d115e68c2a8ab63a680e71ba8901f69c4a1c7a
Sofija Dimitrijevic <dimitrijevic.sofijaa@gmail.com>
2018-03-20 15:30:39 +0100

    Napravljene klase Kurs, Valuta i Menjacnica sa odgovarajucim privatnim atributima. Postavljene get i set metode za atribute.

---
+++ Menjacnica/src/menjacnica/Kurs.java
@@ -0,0 +1,43 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Kurs {
+	private GregorianCalendar datum;
+	private double kupovni;
+	private double prodajni;
+	private double srednji;
+
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+
+	public double getKupovni() {
+		return kupovni;
+	}
+
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+
+	public double getProdajni() {
+		return prodajni;
+	}
+
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+
+	public double getSrednji() {
+		return srednji;
+	}
+
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+
+}
---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,15 @@
+package menjacnica;
+
+import java.util.LinkedList;
+
+public class Menjacnica {
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
+
+	public LinkedList<Valuta> getValute() {
+		return valute;
+	}
+
+	public void setValute(LinkedList<Valuta> valute) {
+		this.valute = valute;
+	}
+}
